102f4faa1cb5475a625847f5fd73a6fac7e35a3b
Nicolas Torelli <nicolaspenatorelli@gmail.com>
2021-04-20 17:12:32 -0300

    Personalizacao do logo, menu e contatos

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         </div>
       </div>
     </div>
-    <div class="page"><a class="section section-banner d-none d-xl-block" href="https://www.templatemonster.com/intense-multipurpose-html-template.html" style="background-image: url(images/banner/background-04-1920x60.jpg); background-image: -webkit-image-set( url(images/banner/background-04-1920x60.jpg) 1x, url(images/banner/background-04-3840x120.jpg) 2x )"><img src="images/banner/foreground-04-1600x60.png" srcset="images/banner/foreground-04-1600x60.png 1x, images/banner/foreground-04-3200x120.png 2x" alt="" width="1600" height="310"></a>
+    <div class="page">
       <!-- Page Header-->
       <header class="section page-header">
         <!-- RD Navbar-->
@@ -37,7 +37,7 @@
                   <!-- RD Navbar Toggle-->
                   <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
                   <!-- RD Navbar Brand-->
-                  <div class="rd-navbar-brand"><a class="brand" href="index.html"><img src="images/logo-default-196x47.png" alt="" width="196" height="47"/></a></div>
+                  <div class="rd-navbar-brand"><a title="Ir para Página Inicial" class="brand" href="index.html"><img src="images/logo-default-196x47.png" alt="" width="196" height="47"/></a></div>
                 </div>
                 <div class="rd-navbar-main-element">
                   <div class="rd-navbar-nav-wrap">
@@ -46,15 +46,15 @@
                       <button class="rd-navbar-basket fl-bigmug-line-shopping198" data-rd-navbar-toggle=".cart-inline"><span>2</span></button>
                       <div class="cart-inline">
                         <div class="cart-inline-header">
-                          <h5 class="cart-inline-title">In cart:<span> 2</span> Products</h5>
-                          <h6 class="cart-inline-title">Total price:<span> $800</span></h6>
+                          <h5 class="cart-inline-title">Carrinho:<span> 2</span> Produtos</h5>
+                          <h6 class="cart-inline-title">Valor total:<span> $800</span></h6>
                         </div>
                         <div class="cart-inline-body">
                           <div class="cart-inline-item">
                             <div class="unit align-items-center">
                               <div class="unit-left"><a class="cart-inline-figure" href="#"><img src="images/product-mini-1-108x100.png" alt="" width="108" height="100"/></a></div>
                               <div class="unit-body">
-                                <h6 class="cart-inline-name"><a href="#">Blueberries</a></h6>
+                                <h6 class="cart-inline-name"><a href="#">Nome Produto</a></h6>
                                 <div>
                                   <div class="group-xs group-inline-middle">
                                     <div class="table-cart-stepper">
@@ -70,7 +70,7 @@
                             <div class="unit align-items-center">
                               <div class="unit-left"><a class="cart-inline-figure" href="#"><img src="images/product-mini-2-108x100.png" alt="" width="108" height="100"/></a></div>
                               <div class="unit-body">
-                                <h6 class="cart-inline-name"><a href="#">Avocados</a></h6>
+                                <h6 class="cart-inline-name"><a href="#">Nome Produto</a></h6>
                                 <div>
                                   <div class="group-xs group-inline-middle">
                                     <div class="table-cart-stepper">
@@ -84,7 +84,7 @@
                           </div>
                         </div>
                         <div class="cart-inline-footer">
-                          <div class="group-sm"><a class="button button-md button-default-outline-2 button-wapasha" href="#">Go to cart</a><a class="button button-md button-primary button-pipaluk" href="#">Checkout</a></div>
+                          <div class="group-sm"><a class="button button-md button-default-outline-2 button-wapasha" href="#">Ver Pedido</a><a class="button button-md button-primary button-pipaluk" href="#">Pagamentos</a></div>
                         </div>
                       </div>
                     </div><a class="rd-navbar-basket rd-navbar-basket-mobile fl-bigmug-line-shopping198" href="#"><span>2</span></a>
@@ -93,7 +93,7 @@
                       <button class="rd-navbar-search-toggle" data-rd-navbar-toggle=".rd-navbar-search"><span></span></button>
                       <form class="rd-search" action="#">
                         <div class="form-wrap">
-                          <label class="form-label" for="rd-navbar-search-form-input">Search...</label>
+                          <label class="form-label" for="rd-navbar-search-form-input">Procurar...</label>
                           <input class="rd-navbar-search-form-input form-input" id="rd-navbar-search-form-input" type="text" name="search">
                           <button class="rd-search-form-submit fl-bigmug-line-search74" type="submit"></button>
                         </div>
@@ -101,13 +101,13 @@
                     </div>
                     <!-- RD Navbar Nav-->
                     <ul class="rd-navbar-nav">
-                      <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Home</a>
+                      <li class="rd-nav-item active"><a class="rd-nav-link" href="index.php">Home</a>
                       </li>
-                      <li class="rd-nav-item"><a class="rd-nav-link" href="about-us.html">About Us</a>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="sobre.php">Sobre</a>
                       </li>
-                      <li class="rd-nav-item"><a class="rd-nav-link" href="typography.html">Typography</a>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="login.php">Login</a>
                       </li>
-                      <li class="rd-nav-item"><a class="rd-nav-link" href="contact-us.html">Contact Us</a>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="contatos.php">Contatos</a>
                       </li>
                     </ul>
                   </div>
@@ -120,24 +120,22 @@
                 </div>
                 <div class="rd-navbar-project rd-navbar-modern-project">
                   <div class="rd-navbar-project-modern-header">
-                    <h4 class="rd-navbar-project-modern-title">Get in Touch</h4>
+                    <h4 class="rd-navbar-project-modern-title">Contate-nos</h4>
                     <div class="rd-navbar-project-hamburger" data-multitoggle=".rd-navbar-main" data-multitoggle-blur=".rd-navbar-wrap" data-multitoggle-isolate>
                       <div class="project-close"><span></span><span></span></div>
                     </div>
                   </div>
                   <div class="rd-navbar-project-content rd-navbar-modern-project-content">
                     <div>
-                      <p>We are always ready to provide you with fresh organic products for your home or office. Contact us to find out how we can help you.</p>
-                      <div class="heading-6 subtitle">Our Contacts</div>
-                      <div class="row row-10 gutters-10">
-                        <div class="col-12"><img src="images/home-sidebar-394x255.jpg" alt="" width="394" height="255"/>
-                        </div>
-                      </div>
+                      <p>Siga-nos em nossas redes sociais e fique por dentro das promoções, participe para ganhar prêmios e grandes descontos!!</p>
+                      <div class="heading-6 subtitle">Siga-nos</div>
+                      
+                      
                       <ul class="rd-navbar-modern-contacts">
                         <li>
                           <div class="unit unit-spacing-sm">
-                            <div class="unit-left"><span class="icon fa fa-phone"></span></div>
-                            <div class="unit-body"><a class="link-phone" href="tel:#">+1 323-913-4688</a></div>
+                            <div class="unit-left"><span class="icon fab fa-whatsapp"></span></div>
+                            <div class="unit-body"><a target="_blank" title="Ir para o Whatsapp" class="link-phone" href="http://api.whatsapp.com/send?1=pt_BR&phone=5514998988662">14 99898-8662</a></div>
                           </div>
                         </li>
                         <li>
@@ -673,3 +671,4 @@
     <!-- coded by Ragnar-->
   </body>
 </html>
+
